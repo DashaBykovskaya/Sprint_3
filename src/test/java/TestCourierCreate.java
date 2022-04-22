cd3d3473fb910d841f37784b96812bf3fa88b3e8
BykovskayaDV <dbykovskaya.db@gmail.com>
2022-04-22 11:14:25 +0300

    Sprint 3

--- src/test/java/TestCourierCreate.java
+++ src/test/java/TestCourierCreate.java
@@ -1,2 +1,48 @@
-package PACKAGE_NAME;public class TestCourierCreate {
+import io.qameta.allure.junit4.DisplayName;
+import io.restassured.RestAssured;
+import model.CreateCourier;
+import org.junit.Before;
+import org.junit.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.equalTo;
+
+public class TestCourierCreate {
+
+    @Before
+    public void setUp() {
+        RestAssured.baseURI = "http://qa-scooter.praktikum-services.ru/";
+    }
+
+    @Test
+    @DisplayName("Create Courier")
+    public void createCourierTest() {
+        CreateCourier courier = new CreateCourier("Test9_", "Qwerty", "Adam");
+        given().header("Content-type", "application/json").and().body(courier).when().post("api/v1/courier").then().assertThat().statusCode(201).and().body("ok", equalTo(true));
+    }
+
+    @Test
+    @DisplayName("Create Courier Simple Name")
+    public void createCourierSimpleNameTest() {
+        CreateCourier courier = new CreateCourier("Test1", "Qwerty123", "Mila");
+        String expected = "Этот логин уже используется. Попробуйте другой.";
+        given().header("Content-type", "application/json").and().body(courier).when().post("api/v1/courier").then().assertThat().statusCode(409).and().body("message", equalTo(expected));
+    }
+
+    @Test
+    @DisplayName("Create Courier With Out Password")
+    public void createCourierWithOutPasswordTest() {
+        CreateCourier courier = new CreateCourier("Test2", "", "Mimi");
+        String expected = "Недостаточно данных для создания учетной записи";
+        given().header("Content-type", "application/json").and().body(courier).when().post("api/v1/courier").then().assertThat().statusCode(400).and().body("message", equalTo(expected));
+    }
+
+    @Test
+    @DisplayName("Create Courier With Out Login")
+    public void createCourierWithOutLoginTest() {
+        CreateCourier courier = new CreateCourier("", "Qwerty", "Mi");
+        String expected = "Недостаточно данных для создания учетной записи";
+        given().header("Content-type", "application/json").and().body(courier).when().post("api/v1/courier").then().assertThat().statusCode(400).and().body("message", equalTo(expected));
+    }
+
 }
--- src/test/java/TestCourierLogin.java
+++ src/test/java/TestCourierLogin.java
@@ -1,2 +1,58 @@
-package PACKAGE_NAME;public class TestCourierLogin {
+import io.qameta.allure.junit4.DisplayName;
+import io.restassured.RestAssured;
+import model.CreateCourier;
+import model.LoginCourier;
+import org.junit.Before;
+import org.junit.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.equalTo;
+
+public class TestCourierLogin {
+
+    @Before
+    public void setUp() {
+        RestAssured.baseURI = "http://qa-scooter.praktikum-services.ru/";
+        CreateCourier courier = new CreateCourier("Testik", "Qwerty", "Adam");
+        given().header("Content-type", "application/json").and().body(courier).when().post("api/v1/courier");
+    }
+
+    @Test
+    @DisplayName("Логин курьера в системе")
+    public void loginCourierTest() {
+        LoginCourier loginCourier = new LoginCourier("Testik", "Qwerty");
+        given().header("Content-type", "application/json").and().body(loginCourier).when().post("/api/v1/courier/login").then().assertThat().statusCode(200).and().assertThat().extract().path("id");
+    }
+
+    @Test
+    @DisplayName("Логин курьера без указания login")
+    public void loginCourierWithOutLoginTest() {
+        LoginCourier loginCourier = new LoginCourier("", "Qwerty");
+        String expected = "Недостаточно данных для входа";
+        given().header("Content-type", "application/json").and().body(loginCourier).when().post("/api/v1/courier/login").then().assertThat().statusCode(400).and().body("message", equalTo(expected));
+    }
+
+    @Test
+    @DisplayName("Логин курьера без пароля")
+    public void loginCourierWithOutPasswordTest() {
+        LoginCourier loginCourier = new LoginCourier("Testik", "");
+        String expected = "Недостаточно данных для входа";
+        given().header("Content-type", "application/json").and().body(loginCourier).when().post("/api/v1/courier/login").then().assertThat().statusCode(400).and().body("message", equalTo(expected));
+    }
+
+    @Test
+    @DisplayName("Логин курьера с login, которого нет в базе")
+    public void loginCourierWrongLoginTest() {
+        LoginCourier loginCourier = new LoginCourier("Пример", "Qwerty");
+        String expected = "Учетная запись не найдена";
+        given().header("Content-type", "application/json").and().body(loginCourier).when().post("/api/v1/courier/login").then().assertThat().statusCode(404).and().body("message", equalTo(expected));
+    }
+
+    @Test
+    @DisplayName("Логин курьера с неверным паролем")
+    public void loginCourierWrongPasswordTest() {
+        LoginCourier loginCourier = new LoginCourier("Testik", "Любовь");
+        String expected = "Учетная запись не найдена";
+        given().header("Content-type", "application/json").and().body(loginCourier).when().post("/api/v1/courier/login").then().assertThat().statusCode(404).and().body("message", equalTo(expected));
+    }
 }
